01ca1799f66da4126b26797237b5e420efc33eb8
Farhan <farhansarang7@gmail.com>
2025-02-17 18:20:07 -0500

    Inital Commit.

---
+++ app/src/main/java/ticket/booking/services/UserBookingService.java
@@ -0,0 +1,11 @@
+package ticket.booking.services;
+import ticket.booking.entities.*;
+
+public class UserBookingService {
+    private User user;
+
+    //Methods
+    public UserBookingService(User user) {
+
+    }
+}
